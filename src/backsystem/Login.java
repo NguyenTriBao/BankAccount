2e47fe91691a5a35dd5220ff6f7b63cd245b884a
NguyenTriBao <tribao0510@gmail.com>
2024-01-17 22:39:28 +0700

    first commit

---
+++ src/backsystem/Login.java
@@ -0,0 +1,33 @@
+
+package backsystem;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+ 
+public class Login extends JFrame {
+    Login(){
+        setLayout(null);
+        
+        setTitle("AUTOMATED TELLER MACHINE ");
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        
+        JLabel label = new JLabel(i3);
+        label.setBounds(70, 10, 100, 100);
+        add(label);
+        
+        getContentPane().setBackground(Color.WHITE);
+        
+        setSize(800, 400);
+        setVisible(true);
+        setLocation(350,200);
+    }
+    
+    public static void main(String args[]){
+        new Login();
+    }
+}
+
